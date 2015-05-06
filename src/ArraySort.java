86db2c9d2956a7ef4c022abecfcfc47a839c3073
Влад <disastersonic@mail.ru>
2015-05-06 14:12:17 +0300

    Sort

---
+++ src/ArraySort.java
@@ -0,0 +1,77 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Влад on 14.04.2015.
+ */
+public class ArraySort {
+    static double number;
+
+    public static void main(String[] params) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of arrays: ");
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+
+        System.out.println("Enter your arrays: ");
+
+        for (int i = 0; i < n; i++) {
+            a = sc.nextInt();
+        }
+        String s = Arrays.toString(a);
+        System.out.println("Array a is " + s);
+
+        System.out.println();
+        
+    }
+
+    static int[] Sort(int[] arr) {
+        if (arr.length > 1) {
+            int[] left = new int[arr.length / 2];
+            int[] right = new int[arr.length - left.length];
+
+            for (int i = 0; i < left.length; i++) {
+                left[i] = arr[i];
+            }
+            for (int i = 0; i < right.length; i++) {
+                right[i] = arr[left.length + i];
+            }
+            if (left.length > 1)
+                left = Sort(left);
+            if (right.length > 1)
+                right = Sort(right);
+            arr = MergeSort(left, right);
+        }
+        return arr;
+    }
+
+    static int[] MergeSort(int[] left, int[] right) {
+        int[] arr = new int[left.length + right.length];
+        int l = 0;
+        int r = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (r >= right.length) {
+                arr[i] = left[l];
+                l++;
+            } else if (l < left.length && left[l] < right[r]) {
+                arr[i] = left[l];
+                l++;
+            } else {
+                arr[i] = right[r];
+                r++;
+
+                if (l < left.length)
+                    number += left.length - 1;
+            }
+
+        }
+        return arr;
+    }
+
+}
---
+++ src/XOR.java
@@ -0,0 +1,64 @@
+/**
+ * Created by Влад on 14.04.2015.
+ */
+public class XOR {
+    public static void main(String[] params) {
+/*
+        int a = 5;
+        int b = 5;
+        String message = new String();
+        System.out.println();
+*/
+    }
+
+    public static String crypt(String message, char[] key) {
+
+
+
+
+
+
+
+        /*
+        char[] code = message.toCharArray();
+        for (int i = 0; i < code.length; i++) {
+            code[i] = (char) (code[i] ^ key[i % key.length]);
+            System.out.println("Исходная строка" + message);
+            char[] chars = message.toCharArray();
+            String someString = String.copyValueOf(chars);
+
+*/
+            //char crypedChar ^= key;
+            return message;
+
+        }
+
+
+        //message = new String(chars);
+        //return message;
+
+        /*
+        String txt = message.getBytes();
+        String rkey = key.getBytes();
+        String res = new byte[message.length()];
+
+        for (int i = 0; i < txt.length; i++) {
+            res[i] = (byte) (txt[i] ^ key[i % key.length]);
+        }
+
+        return res;
+
+        char[] chars = message.toCharArray();
+        for (int i = 0, a = 0; i < chars.length; i++, a++) {
+            chars[i] = (char) (chars[i] ^ key[a]);
+            if (a == key.length - 1) {
+                a = 0;
+*/
+
+        //return message;
+    }
+
+
+
+
+
