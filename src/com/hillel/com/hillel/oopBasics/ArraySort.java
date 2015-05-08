f1d8637d1c1c7fff66728bcaae69548169fff898
Влад <disastersonic@mail.ru>
2015-05-08 15:59:52 +0300

    Sort

--- src/com/hillel/com/hillel/oopBasics/ArraySort.java
+++ src/com/hillel/com/hillel/oopBasics/ArraySort.java
@@ -1,16 +1,22 @@
+package com.hillel.com.hillel.oopBasics;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Random;
 
 /**
- * Created by Влад on 14.04.2015.
+ * Created by ???? on 14.04.2015.
  */
 public class ArraySort {
-    static double number;
-
 
     public static void main(String[] params) {
-        int[] arr = {1, 3, 5, 2, 6, 4};
+        //int[] arr = {1, 3, 5, 2, 6, 4};
+        int[] arr;
+        arr = new int[10];
+        Random rand = new Random();
+        for(int i = 0; i < 10; i++){
+          arr[i] = rand.nextInt(50);
+        }
         System.out.println("Array: ");
         Arrays.toString(arr);
         System.out.print(Arrays.toString(arr));
@@ -21,40 +27,14 @@ public class ArraySort {
         Arrays.toString(arr);
         System.out.print(Arrays.toString(arr));
 
-
-       /* Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter number of arrays: ");
-
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        System.out.println("Enter your arrays: ");
-
-        for (int i = 0; i < n; i++) {
-            arr = sc.nextInt();
-        }
-        String s = Arrays.toString(arr);
-        System.out.println(s);
-
-        System.out.println();
-       */
     }
 
     static int[] sort(int[] arr) {
 
         if (arr.length > 1) {
-            int[] left = new int[arr.length / 2];
-            int[] right = new int[arr.length - left.length];
 
-            //int[] part1 = Arrays.copyOfRange(arr,0,left.length);
-           // int[] part2 = Arrays.copyOfRange(arr,0,right.length);
-            for (int i = 0; i < left.length; i++) {
-                left[i] = arr[i];
-            }
-            for (int i = 0; i < right.length; i++) {
-                right[i] = arr[left.length + i];
-            }
+            int[] left = Arrays.copyOfRange(arr, 0, arr.length / 2);
+            int[] right = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
             if (left.length > 1)
                 left = sort(left);
             if (right.length > 1)
