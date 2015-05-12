6bcb8e298176b44079e0c0a6d8055e31549cc98d
Влад <disastersonic@mail.ru>
2015-05-12 23:57:36 +0300

    Sort

--- src/XOR.java
+++ src/XOR.java
@@ -1,62 +1,32 @@
+
+
+
 /**
  * Created by Влад on 14.04.2015.
  */
 public class XOR {
     public static void main(String[] params) {
-/*
-        int a = 5;
-        int b = 5;
-        String message = new String();
-        System.out.println();
-*/
+
+        String message = new String("Rainbow");
+        char key = 1;
+        System.out.println("Message: " + message);
+        String cryptedMessage = crypt(message,key);
+        System.out.println("Crypted message: " + cryptedMessage);
+        String cryptedMessageIsCryptedAgain = crypt(cryptedMessage,key);
+        System.out.println("Crypted message crypted: " + cryptedMessageIsCryptedAgain);
     }
 
-    public static String crypt(String message, char[] key) {
-
-
-
-
-
-
-
-        /*
-        char[] code = message.toCharArray();
-        for (int i = 0; i < code.length; i++) {
-            code[i] = (char) (code[i] ^ key[i % key.length]);
-            System.out.println("Исходная строка" + message);
-            char[] chars = message.toCharArray();
-            String someString = String.copyValueOf(chars);
-
-*/
-            //char crypedChar ^= key;
-            return message;
-
+    public static String crypt(String message, char key) {
+        char[] array = message.toCharArray();
+        int l = array.length;
+        for(int i = 0; i < l;i ++){
+         array[i] ^= key;
         }
-
-
-        //message = new String(chars);
-        //return message;
-
-        /*
-        String txt = message.getBytes();
-        String rkey = key.getBytes();
-        String res = new byte[message.length()];
-
-        for (int i = 0; i < txt.length; i++) {
-            res[i] = (byte) (txt[i] ^ key[i % key.length]);
-        }
-
-        return res;
-
-        char[] chars = message.toCharArray();
-        for (int i = 0, a = 0; i < chars.length; i++, a++) {
-            chars[i] = (char) (chars[i] ^ key[a]);
-            if (a == key.length - 1) {
-                a = 0;
-*/
-
-        //return message;
+        message = String.copyValueOf(array);
+        return message;
     }
+
+}
 
 
 
