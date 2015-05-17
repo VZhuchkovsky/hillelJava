ae3c6b1eeeea2926138e04bebafdb60fb10592ad
Влад <disastersonic@mail.ru>
2015-05-17 17:02:17 +0300

    Sort

--- src/XOR.java
+++ src/XOR.java
@@ -8,11 +8,11 @@ public class XOR {
     public static void main(String[] params) {
 
         String message = new String("Rainbow");
-        char[] key = {1,2};
+        char[] key = {1, 2};
         System.out.println("Message: " + message);
-        String cryptedMessage = crypt(message,key);
+        String cryptedMessage = crypt(message, key);
         System.out.println("Crypted message: " + cryptedMessage);
-        String cryptedMessageIsCryptedAgain = crypt(cryptedMessage,key);
+        String cryptedMessageIsCryptedAgain = crypt(cryptedMessage, key);
         System.out.println("Crypted message crypted: " + cryptedMessageIsCryptedAgain);
     }
 
@@ -20,10 +20,17 @@ public class XOR {
         char[] array = message.toCharArray();
         int l = array.length;
         int o = key.length;
-        for(int i = 0; i < l;i ++){
-          for(int j = 0; j < o; j++){
-              array[i] ^= key[j];
-          }
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < o; j++) {
+                if (i % o == 0) {
+                    j = 0;
+                    j++;
+                    array[i] ^= key[j];
+                } else {
+                    array[i] ^= key[j];
+                }
+
+            }
         }
         message = String.copyValueOf(array);
         return message;
